f99497c6a52a9ddc372c455c4f21b195ecf33a2f
Jeffrey Renza <funk101@gmail.com>
2018-09-26 00:03:55 -0400

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,36 +1,16 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = { id: 10 }
-var token = jwt.sign(data, '123abc');
-console.log(token);
+var password = '123abc!';
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded ', decoded);
+/* bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+}); */
 
-
-/* var message = 'I am user number 3';
-var hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
-
-var data = {
-    id: 4
-};
-
-var token = {
-    data,
-    hash: SHA256(JSON.stringify(data) + 'somesecret').toString()
-};
-
-token.data.id = 5;
-token.hash = SHA256(JSON.stringify(token.data)).toString();
-
-var resultHash = SHA256(JSON.stringify(token.data) + 'somesecret').toString();
-
-if (resultHash === token.hash) {
-    console.log('Data was not changed');
-} else {
-    console.log('Data was changed don\'t trust');
-} */
+var hashedPassword = '$2a$10$1M5C/at49glUvqfNrffFs.dc5AbZUpjgyBn3xH.okjWPbatr1DWZ.';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
