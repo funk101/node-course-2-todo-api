452d3f71599830e4623ae4638324265b1850b721
Jeffrey Renza <funk101@gmail.com>
2018-10-27 10:16:02 -0400

    add tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,6 +1,7 @@
 const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
+const {User} = require('../models/user');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
@@ -170,3 +171,76 @@ describe('PATCH /todos/:id', () => {
   });
 });
 
+describe('GET /users/me', () => {
+  it ('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it ('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+    .end(done);
+    });
+});
+
+describe('POST /users', () => {
+  it ('should create a user', (done) => {
+    var email = 'test@testco.com';
+    var password = '123mnb!';
+
+    request(app)
+      .post('/users')
+      .send({email, password})
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toBeTruthy();
+        expect(res.body._id).toBeTruthy();
+        expect(res.body.email).toBe(email);
+      })
+      .end((err) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findOne({email}).then((user) => {
+          expect(user).toBeTruthy();
+          expect(user.password).not.toBe(password);
+          done();
+        });
+      });
+  });
+
+  it ('should return validation errors if request invalid', (done) => {
+    var email = 'test2';
+    var password = '13984';
+
+    request (app)
+      .post('/users')
+      .send({email,password})
+      .expect(400)
+      .end(done);
+  });
+
+  it ('should not create user if email in use', (done) => {
+    var email = users[0].email;
+    var password = 'asasas';
+    request (app)
+      .post('/users')
+      .send({email,password})
+      .expect(400)
+      .end(done)
+  });
+
+});
